fba19163957477ae781380e590cc1a2cfedd1557
Paul Carey <paul.p.carey@gmail.com>
2008-08-23 12:05:31 +0100

    Removed inspect - was causing problems

--- lib/relaxdb/all_delegator.rb
+++ lib/relaxdb/all_delegator.rb
@@ -26,7 +26,7 @@ module RelaxDB
     
     # Note that this method leaves the corresponding DesignDoc for this class intact
     def destroy!
-      each do |o| 
+      self.each do |o| 
         # A reload is required for deleting objects with a self referential references_many relationship
         # when a cache is not used. This makes destroy_all! very slow. Given that references_many is
         # now deprecated and will soon be removed, the required reload is no longer performed.
@@ -37,10 +37,14 @@ module RelaxDB
       end
     end
     
-    # Defined so this may be used in irb
-    def inspect
-      @all ? @all : "uninitialized"
+    def foo
+      "foo"
     end
+    
+    # Defined so this may be used in irb
+    # def inspect
+    #   @all ? @all : "uninitialized"
+    # end
     
   end
   
